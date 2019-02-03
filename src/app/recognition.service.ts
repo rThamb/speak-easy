b86976a9c391acccb42c058f112f06471e7cd610
Renuchan Thambirajah <renuchan.thamb@gmail.com>
2019-02-03 12:28:47 -0500

    Speech to Text Working.

--- src/app/recognition.service.ts
+++ src/app/recognition.service.ts
@@ -1,13 +1,75 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as _ from "lodash";
+
+interface IWindow extends Window {
+    webkitSpeechRecognition: any;
+    SpeechRecognition: any;
+}
+
+
+//Reference: https://hassantariqblog.wordpress.com/2016/12/04/angular2-web-speech-api-speech-recognition-in-angular2/
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecognitionService {
 
-  constructor() { }
+  speechRecognition: any;
+
+  constructor(private zone: NgZone) { }
 
   getData(){
     return "Hello World"; 
+  }
+
+  //pass the language the speaker is talking in. 
+  listen(): Observable<string> {
+
+        return Observable.create(observer => {
+            const { webkitSpeechRecognition }: IWindow = <IWindow>window;
+            this.speechRecognition = new webkitSpeechRecognition();
+            //this.speechRecognition = SpeechRecognition;
+            this.speechRecognition.continuous = true;
+            //this.speechRecognition.interimResults = true;
+            this.speechRecognition.lang = 'en-us';
+            this.speechRecognition.maxAlternatives = 1;
+
+            this.speechRecognition.onresult = speech => {
+                let term: string = "";
+                if (speech.results) {
+                    var result = speech.results[speech.resultIndex];
+                    var transcript = result[0].transcript;
+                    if (result.isFinal) {
+                        if (result[0].confidence < 0.3) {
+                            console.log("Unrecognized result - Please try again");
+                        }
+                        else {
+                            term = _.trim(transcript);
+                            console.log("You said: -> " + term);
+                        }
+                    }
+                }
+                this.zone.run(() => {
+                    observer.next(term);
+                });
+            };
+
+            this.speechRecognition.onerror = error => {
+                observer.error(error);
+            };
+
+            this.speechRecognition.onend = () => {
+                observer.complete();
+            };
+
+            this.speechRecognition.start();
+            console.log("Say something - We are listening !!!");
+        });
+    }
+
+  DestroySpeechObject() {
+      if (this.speechRecognition)
+            this.speechRecognition.stop();
   }
 }
--- src/app/speak/speak.component.ts
+++ src/app/speak/speak.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {RecognitionService} from '../recognition.service';
 
 @Component({
@@ -6,23 +6,62 @@ import {RecognitionService} from '../recognition.service';
   templateUrl: './speak.component.html',
   styleUrls: ['./speak.component.css']
 })
-export class SpeakComponent implements OnInit {
+export class SpeakComponent implements OnInit, OnDestroy {
 
   service: RecognitionService;
-  message = ""; 
+  
+  speechData: string;
+
+  langSpoken: string; 
+  langTranslated: string; 
 
   //Angular will inject the custom service, make sure to register app in app.module.ts (providers)
+  //ng generate service {serviceName} - to generate a new service
   constructor(private sttService: RecognitionService) {
-
-
     this.service = sttService;
   }
 
   ngOnInit() {
-    this.message = this.service.getData();
+    this.speechData = "";
+    this.langSpoken = ""; 
+    this.langTranslated = ""; 
+  }
+
+  ngOnDestroy() {
+    this.service.DestroySpeechObject();
   }
 
   listen(){
-    alert("Test");
+    this.activateSpeechSearchMovie();
   }
+
+
+  activateSpeechSearchMovie(): void {
+        this.service.listen()
+            .subscribe(
+            //listener
+            (value) => {
+                this.speechData = value;
+                alert(value);
+                console.log(value);
+            },
+            //errror
+            (err) => {
+                console.log(err);
+                if (err.error == "no-speech") {
+                    console.log("--restatring service--");
+                    this.activateSpeechSearchMovie();
+                }
+            },
+            //completion
+            () => {
+                console.log("--complete--");
+                this.activateSpeechSearchMovie();
+            });
+    }
+
+    translateSpeech(speechData){
+        //use translate service
+    }
+
 }
